cd80e5aff5395854c7d36f494b39c3e87dc5d93e
Amallubis <amal.lubis@gmail.com>
2023-09-20 09:54:29 +0700

    menambahkan database

---
+++ backend/forms.py
@@ -0,0 +1,27 @@
+from django.forms import ModelForm
+from django import forms
+from backend.models import Berita
+
+
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleFileField(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+class FormPost(forms.Form):
+    file_field=MultipleFileField()
+
+
+        
